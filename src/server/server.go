9ac3f3c405fc2f2efd1050708f79a16c0638466d
ryuta <komori@c-2-d.com>
2019-04-22 02:16:37 +0900

    Front接続確認バージョン

--- src/server/server.go
+++ src/server/server.go
@@ -1,10 +1,17 @@
 package server
 
+import (
+	"context"
+	"log"
+
+	pb "proton"
+)
+
 type Server struct {
 }
 
-func NewServer() (server *Server) {
-	server = &Server{}
-
-	return server
+// SayHello implements helloworld.GreeterServer
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.Name)
+	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
 }
