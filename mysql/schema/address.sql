6a0db7193c2e5d818c70d2565b9c7d44f2df3f19
ryuta <komori@c-2-d.com>
2019-05-04 12:37:31 +0900

    アドレスリスト取得実装

--- mysql/schema/address.sql
+++ mysql/schema/address.sql
@@ -4,3 +4,8 @@ CREATE TABLE `address` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
  )
+
+INSERT INTO `address` (`id`, `email`, `created_at`)
+  VALUES
+	(1, 'test@test.co.jp', '2019-05-04 03:23:39'),
+	(2, 'aaa@aaa.co.jp', '2019-05-04 03:23:46');
--- src/server/address.go
+++ src/server/address.go
@@ -6,32 +6,19 @@ import (
 )
 
 func (s *Server) ListAddress(ctx context.Context, req *pb.ListAddressRequest) (res *pb.ListAddressResponse, err error) {
+	address_list, err := s.model.AllFindAddress()
 
-	// company := s.model.NewClinotCompany()
-
-	// if err := company.Find(sq.Eq{"uuid": req.Uuid}); err != nil {
-	// 	return nil, status.Error(codes.NotFound, "The clinot_company not found")
-	// }
-
-	// ClinotAddresses, err := s.model.FindClinotAddressUuid(company.ID)
-
-	// var addresses []*proton.ClinotAddress
-
-	// if ClinotAddresses == nil {
-	// 	return &proton.ListClinotAddressResponse{}, nil
-	// }
-
-	// for _, ca := range ClinotAddresses {
-	// 	addresses = append(addresses, &proton.ClinotAddress{
-	// 		Id:              int32(ca.ID),
-	// 		ClinotCompanyId: int32(ca.ClinotCompanyID),
-	// 		Email:           ca.Email},
-	// 	)
-	// }
+	if address_list == nil {
+		return &pb.ListAddressResponse{}, nil
+	}
 	var addresses []*pb.Address
 
-	addresses = append(addresses, &pb.Address{Id: 1, Email: "test@test.co.jp"})
-	// res = &proton.ListClinotAddressResponse{Name: company.Name, ClinotAddresses: addresses}
+	for _, val := range address_list {
+		addresses = append(addresses, &pb.Address{
+			Id:    uint32(val.ID),
+			Email: val.Email},
+		)
+	}
 
 	res = &pb.ListAddressResponse{Address: addresses}
 	return res, nil
